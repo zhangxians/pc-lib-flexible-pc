1a91170e20984672bbfc50936b3b41a7bb7a529b
张先生 <1831323502@qq.com>
2020-09-29 14:11:34 +0800

    '修改px比例'

--- flexible.js
+++ flexible.js
@@ -69,9 +69,9 @@
     function refreshRem(){
         var width = docEl.getBoundingClientRect().width;
         if (width / dpr > 540) {
-            width = 540 * dpr;
+            width = width * dpr;
         }
-        var rem = width / 10;
+        var rem = width / 137.1428;//使用1920标准屏
         docEl.style.fontSize = rem + 'px';
         flexible.rem = win.rem = rem;
     }
